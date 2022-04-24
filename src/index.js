9e84cce6478ef5d55a76a0663ba2e3153b1ceaca
f2xiao <fxiao.mecheng@gmail.com>
2022-04-24 17:39:29 -0400

    Edit Board and Square class

--- src/index.js
+++ src/index.js
@@ -6,28 +6,36 @@ import './index.css';
  * Render a single square (button) 
  * 
  */
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-    render() {
-      return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
-        </button>
-      );
-    }
-  }
+const Square = function (props) {
+  return (
+    <button className="square" onClick={()=>props.onClick()}>
+      {props.value}
+    </button>
+  );
+}
 
 /**
  * Render 9 squares
  */
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+             />;
   }
   
   render() {
